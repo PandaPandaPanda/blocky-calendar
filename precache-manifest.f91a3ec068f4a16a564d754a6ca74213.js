9a9c358e46de942e15dd1ff83777a753fec53ab5
PandaPandaPanda <34975856+PandaPandaPanda@users.noreply.github.com>
2021-10-10 22:17:34 +0800

    Updates

---
+++ precache-manifest.f91a3ec068f4a16a564d754a6ca74213.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c956b36b55107f0f8f8d35d40180e873",
+    "url": "/blocky-calendar/index.html"
+  },
+  {
+    "revision": "0de4971d371831b22964",
+    "url": "/blocky-calendar/static/css/2.0f07b481.chunk.css"
+  },
+  {
+    "revision": "16fdec29d4f09b70baaf",
+    "url": "/blocky-calendar/static/css/main.a8c5c7c8.chunk.css"
+  },
+  {
+    "revision": "0de4971d371831b22964",
+    "url": "/blocky-calendar/static/js/2.ec972b72.chunk.js"
+  },
+  {
+    "revision": "8403fba1f5196e6eed2e2de682ad8a22",
+    "url": "/blocky-calendar/static/js/2.ec972b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16fdec29d4f09b70baaf",
+    "url": "/blocky-calendar/static/js/main.59baa7d6.chunk.js"
+  },
+  {
+    "revision": "2dacf1b8653e916604f7",
+    "url": "/blocky-calendar/static/js/runtime-main.150cdfae.js"
+  },
+  {
+    "revision": "aff79bda83b220a08ffaebcfffca3f7b",
+    "url": "/blocky-calendar/static/media/Gamepixies-8MO6n.aff79bda.ttf"
+  }
+]);
